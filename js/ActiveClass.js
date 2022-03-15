b1bee5f90347442abfca82fa758ba52e5dc95876
Alemau97 <mauriciomn@hotmail.es>
2022-03-15 13:03:18 -0600

    Animacion en scroll

--- js/ActiveClass.js
+++ js/ActiveClass.js
@@ -9,3 +9,19 @@ window.addEventListener('scroll', function(){
         menu.classList.remove('fixed');
     }
 })
+
+//Scroll on elements
+let animado = document.querySelectorAll(".animado");
+
+function mostrarScroll(){
+    let scrollTop = document.documentElement.scrollTop;
+    for(var i=0; i < animado.length; i++){
+        let  alturaAnimado = animado[i].offsetTop;
+        if(alturaAnimado - 680 < scrollTop){
+            animado[i].style.opacity = 1;
+            animado[i].classList.add("animado_scroll");
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll);
